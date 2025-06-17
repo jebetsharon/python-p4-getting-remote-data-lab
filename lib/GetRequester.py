c7b55ef828c5f32def799ce9249c5fe125e005fe
Sharon kosgei <jebetsharon089@gmail.com>
2025-06-17 09:10:10 +0300

    comlete get request

--- lib/GetRequester.py
+++ lib/GetRequester.py
@@ -7,7 +7,11 @@ class GetRequester:
         self.url = url
 
     def get_response_body(self):
-        pass
+        response = requests.get(self.url)
+        response.raise_for_status()
+        return response.content
 
     def load_json(self):
-        pass
+        body_bytes = self.get_response_body()
+        body_str = body_bytes.decode('utf-8')
+        return json.loads(body_str)
